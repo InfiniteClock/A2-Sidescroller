7ce1c557fd4ffda3b5d4268c15b2e099d0f450e7
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2024-11-16 23:36:06 -0500

    Task 3 Complete
    Added jumping and grounded-checking functionality. Adjusted movement parameters to feel more to preference

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,12 @@ public class PlayerController : MonoBehaviour
     public float topSpeed;
     public float accelSpeed;
     public float decelTime;
+    public float AirDrag;
+    public float jumpForce;
 
+    public Vector2 boxSize;
+    public float boxOffset;
+    public LayerMask nonGround;
 
     private Rigidbody2D rb;
     private Vector2 playerInput;
@@ -41,6 +46,13 @@ public class PlayerController : MonoBehaviour
         {
             playerInput += Vector2.right;
         }
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            if (IsGrounded())
+            {
+                playerInput += Vector2.up;
+            }
+        }
     }
 
     private void MovementUpdate(Vector2 playerInput)
@@ -48,12 +60,25 @@ public class PlayerController : MonoBehaviour
         // Decelerate the player if input direction is 0
         if (playerInput.x == 0)
         {
-            rb.velocity += new Vector2(-rb.velocity.x / decelTime, 0);
+            if (IsGrounded())
+            {
+                rb.velocity += new Vector2(-rb.velocity.x / decelTime, 0);
+            }
+            else
+            {
+                rb.velocity += new Vector2(-rb.velocity.x / AirDrag, 0);
+            }
         }
         // Accelerate the player in intended direction towards top speed
         else if (rb.velocity.x < topSpeed && rb.velocity.x > -topSpeed)
         {
             rb.AddForce(new Vector2(playerInput.x * accelSpeed, 0));
+        }
+
+        if (playerInput.y > 0)
+        {
+            // Jump will use impulse mode for a snappier jump
+            rb.AddForce(new Vector2(0, playerInput.y * jumpForce), ForceMode2D.Impulse);
         }
     }
 
@@ -65,8 +90,21 @@ public class PlayerController : MonoBehaviour
         }
         return false;
     }
+
+    private void OnDrawGizmos()
+    {
+        // Display the boxcast in editor
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position - transform.up * boxOffset, boxSize);
+    }
     public bool IsGrounded()
     {
+        // Use a boxcast to detect ground beneath the player
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, boxOffset, nonGround))
+        {
+            return true;
+        }
+        // Returns false if the boxcast fails to detect ground
         return false;
     }
 
