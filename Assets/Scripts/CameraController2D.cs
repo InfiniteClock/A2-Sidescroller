41fcfc38baa0e998065ea7d0ef9e5c9013d71ff5
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2024-12-03 13:35:34 -0500

    Added Camera Shake
    Camera Shake added when pressing 'L'

--- Assets/Scripts/CameraController2D.cs
+++ Assets/Scripts/CameraController2D.cs
@@ -18,6 +18,9 @@ public class CameraController2D : MonoBehaviour
     [SerializeField] private Vector2 offset;
     [SerializeField] private float smoothing = 5f;
 
+    private Vector3 shakeOffset = Vector3.zero;
+    private Coroutine shaking;
+
     void Start()
     {
         tilemap.CompressBounds();
@@ -36,11 +39,30 @@ public class CameraController2D : MonoBehaviour
 
     public void LateUpdate()
     {
-        Vector3 desiredPos = target.position + new Vector3(offset.x, offset.y, transform.position.z);
+        // Testing only
+        if (Input.GetKey(KeyCode.L)) Shake(10f, 2f);
+
+        Vector3 desiredPos = target.position + new Vector3(offset.x, offset.y, transform.position.z) + shakeOffset;
         Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, 1 - Mathf.Exp(-smoothing * Time.deltaTime));
         smoothPos.x = Mathf.Clamp(smoothPos.x, leftBoundLimit, rightBoundLimit);
         smoothPos.y = Mathf.Clamp(smoothPos.y, bottomBoundLimit, smoothPos.y);
 
         transform.position = smoothPos;
+    }
+    public void Shake(float intensity, float duration)
+    {
+        shaking = StartCoroutine(ShakeCoroutine(intensity, duration));
+    }
+
+    private IEnumerator ShakeCoroutine(float intensity, float duration)
+    {
+        float elapsed = 0f;
+        while(elapsed < duration)
+        {
+            shakeOffset = Random.insideUnitCircle * intensity;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        shakeOffset = Vector3.zero;
     }
 }
